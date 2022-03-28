6348c07d12966f0c439715ee47d5eb962fa39f32
Marcos Blanco <blancomarcos.93@gmail.com>
2022-03-28 14:20:11 -0300

    Dimos estructura a las cards

--- src/components/Main/ListProduct/Card/Card.js
+++ src/components/Main/ListProduct/Card/Card.js
@@ -1,18 +1,43 @@
+import React,{useState, useEffect} from 'react'
 import './Card.css'
-import React,{ useState } from 'react'
-import ItemCount from "./ItemCount/ItemCount"
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-function Card({titulo, precio, talle, stock}) {
+function Cards({dataProduct}) {
+  const {title, price, stock, image } = dataProduct
+  const [ count, setCount ] = useState(0)
+  const [ countTest, setCountTest ] = useState(0)
 
-  return(
-    <div className='formatCard'>
-      <h2>{titulo}</h2>
-      <p>Precio: ${precio}</p>
-      <p>Talle: {talle}</p>
-      <p>Stock Disponible: {stock}</p>
-      <ItemCount stock={stock}/>
-    </div>
-  )
+     
+const addStock = () => {
+  setCount(count + 1)
+}
+const removeStock = () => {
+  setCountTest(countTest - 1)
+}
+return (
+    <Card className='formatCard' sx={{ maxWidth: 320, }}>
+      <img className='imgCard' src={`./${image}`} alt={image}/>
+      <CardContent className="itemCard">
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography className="itemPrice" color="text.primary">
+          Precio: ${price}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Cantidad disponible: {stock}
+        </Typography>
+        <div className='buttonCard'>
+          <Button variant="contained" color="success">
+            Agregar al carrito
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default Card
+export default Cards
--- src/components/Main/ListProduct/ListProduct.js
+++ src/components/Main/ListProduct/ListProduct.js
@@ -1,15 +1,72 @@
 import './ListProduct.css'
-import Card from './Card/Card'
+import React,{useState, useEffect} from 'react'
+import Card from "../ListProduct/Card/Card"
 
-const ListProduct = () => {
+const ListProducts = () => {
+    // Creamos una constante para mostrar los productos
+    const mockProducts = [{
+        id: 1,
+        title: "Argentina Titular",
+        price: 8000,
+        stock: 40,
+        image: "argTit.webp"
+    },
+    {
+        id: 2,
+        title: "Argentina Entrenamiento",
+        price: 6000,
+        stock: 15,
+        image: "argAfa.webp"
+    },
+    {
+        id: 3,
+        title: "PSG Titular",
+        price: 10000,
+        stock: 15,
+        image: "psgTit.webp"
+    },
+    {
+        id: 4,
+        title: "PSG Suplente",
+        price: 12000,
+        stock: 20,
+        image: "psgSup.webp"
+    },
+    {
+        id: 5,
+        title: "PSG Alternativa",
+        price: 14000,
+        stock: 15,
+        image: "psgAlt.webp"
+    }]
+
+    const [products, setProducts] = useState ([])
+
+    // Creamos una promesa para que nos devuelva los productos
+    const getProducts = () => {
+        return new Promise ((resolve, reject) => {
+            return resolve (mockProducts)
+        })
+    }
+
+    useEffect(() => {
+        getProducts().then((dataProduct) => { //Con el "then" podemos visualizar los datos de la promesa
+            setProducts(dataProduct) // Acá guardamos los datos en el state de productos.
+        }).finally(() => {console.log("Finalizó la llamada")})
+    }, [])
+
+
     return(
-        <div className="divClass">
-            <Card titulo='PSG' talle='L' precio={2000} stock={10}/>
-            <Card titulo='Barcelona' talle='M' precio={3000} stock={15}/>
-            <Card titulo='Entrenamiento' talle='M' precio={4000} stock={4}/>
-            <Card titulo='Argentina' talle='XL' precio={1500} stock={7}/>
+        <div className='divClass'>
+            {products.map( ( product ) => {
+                const {id} = product
+                return(
+                        <Card dataProduct={product} key={id}/>
+                    
+                )
+            })}
         </div>
     )
 }
 
-export default ListProduct
+export default ListProducts
